2d3c32f73a183053fc8e90c340b99de2d9a87baf
Juliana <71522623+juju-lee@users.noreply.github.com>
2022-11-18 13:52:55 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    $(".bar").each(function(){
+        $(this).find(".bar-inner").animate({
+          width: $(this).attr("data-width")
+        },2000)
+      }); 
+    
+  });
